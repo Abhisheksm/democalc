829a7a2428e079ac9a19071a72c1493d990b8fdb
root <root@localhost.localdomain>
2021-01-17 20:15:27 +0000

    updated

--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example
 import static org.junit.Assert.*;
 import java.io.*;
 import javax.servlet.http.*;
@@ -10,7 +10,7 @@ public class CalculatorTest extends Mockito{
     @Test
     public void testAdd() throws Exception {
 
-        int k= new Calculator().add(10, 12)
+        int k= new Calculator().add(10, 12);
         assertEquals("Problem with Add function:", 22, k);
         
     }
